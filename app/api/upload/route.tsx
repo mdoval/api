c16697adc2e2aa86a8d4327eced77cc4a279ad05
Martinchoe <martindoval@gmail.com>
2024-08-20 16:47:57 -0300

    api working

---
+++ app/api/upload/route.tsx
@@ -0,0 +1,25 @@
+import { NextApiResponse } from "next"
+import { NextResponse } from "next/server"
+import { writeFile } from 'fs/promises'
+import path from 'path'
+
+export async function POST(req: Request, res: NextApiResponse) {            
+    const data = await req.formData()
+    const file: File = data.get('file') as File
+
+    if(!file) {
+        return NextResponse.json({message: "No existe la propiedad File en el formulario enviado"})
+    }
+
+    const bytes = await file.arrayBuffer()
+    const buffer = Buffer.from(bytes)
+
+    const dir: string = process.env.IMAGEPATH as string
+    const url: string = process.env.OUTPUTURL as string
+    const fileName: string = file.name
+
+    const filePath = path.join(dir+fileName)
+    writeFile(filePath, buffer)
+
+    return NextResponse.json({message: "Se subio correctamente el archivo", url: url+fileName})
+}
