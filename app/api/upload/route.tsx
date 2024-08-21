b9f26e1c4888a8a8cbb9d7330fba2cba70836064
Martin Doval <martindoval@gmail.com>
2024-08-21 16:05:40 +0200

    v4 (name)

--- app/api/upload/route.tsx
+++ app/api/upload/route.tsx
@@ -6,17 +6,25 @@ import path from 'path'
 export async function POST(req: Request, res: NextApiResponse) {            
     const data = await req.formData()
     const file: File = data.get('file') as File
+    const fileNameUpload: string = data.get('name') as string
 
     if(!file) {
         return NextResponse.json({status: "Ok", url: "No existe", message: "No existe la propiedad File en el formulario enviado"})
     }
+
 
     const bytes = await file.arrayBuffer()
     const buffer = Buffer.from(bytes)
 
     const dir: string = process.env.IMAGEPATH as string
     const url: string = process.env.OUTPUTURL as string
-    const fileName: string = file.name
+    
+    //asignacion de nombre
+    let fileName: string = file.name    
+    if(fileNameUpload) {
+        fileName = fileNameUpload
+    }
+    //const fileName: string = file.name
 
     const filePath = path.join(dir+fileName)
     writeFile(filePath, buffer)
